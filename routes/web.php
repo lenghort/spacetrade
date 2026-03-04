e7bf800b1b11d9286a4d7419e75ef6db4ae6c95f
lenghort <105147959+lenghort@users.noreply.github.com>
2026-03-05 02:07:42 +0700

    Note structures for routes file

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+ * Public routes 
+ */
+
 Route::get('/', \App\Http\Controllers\welcomecontroller::class)->name('home');
+
+/*
+ * User zone routes
+ */
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 require __DIR__.'/settings.php'; 
+
+/*
+ * Admin zone routes 
+ */
