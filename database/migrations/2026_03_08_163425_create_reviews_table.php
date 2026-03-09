329d9f53281fb957fbf051c1e16d0c14e579e11e
lenghort <105147959+lenghort@users.noreply.github.com>
2026-03-10 03:54:04 +0700

    add author to review

--- database/migrations/2026_03_08_163425_create_reviews_table.php
+++ database/migrations/2026_03_08_163425_create_reviews_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignId('shop_id');
+            $table->foreignId('author_id');
             $table->text('comment');
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(15)->create();
 
         Shop::factory(10)->create();
         Review::factory(30)->create();
