5e9b062d5eee70774413dbef129b3761287824f0
lenghort <105147959+lenghort@users.noreply.github.com>
2026-03-12 19:49:40 +0700

    re-arrange login from welcome blade to site nav

--- resources/views/components/site-navigation.blade.php
+++ resources/views/components/site-navigation.blade.php
@@ -1,6 +1,41 @@
 <nav class="bg-purple-900 text-white">
-    <ul class="flex space-x-3 p-3">
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('shops.index') }}">Shop</a></li>
-    </ul>
+
+    <div class="max-7xl mx-auto flex justify-between item-center">
+        <ul class="flex space-x-3 p-3">
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('shops.index') }}">Shop</a></li>
+        </ul>
+
+    
+        @if (Route::has('login'))
+                    <div class="flex items-center justify-end gap-4">
+                        @auth
+                            <a
+                                href="{{ route('dashboard') }}"
+                                class="">
+                                Dashboard
+                            </a>
+                        @else
+                            <a
+                                href="{{ route('login') }}"
+                                class="">
+                                Log in
+                            </a>
+
+                            @if (Route::has('register'))
+                                <a
+                                    href="{{ route('register') }}"
+                                    class="">
+                                    Register
+                                </a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+        </div>
+
+
+
+
+
 </nav>
