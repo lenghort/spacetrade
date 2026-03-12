7dff99c87c6502cc766abf86395b7c6c19b2571a
lenghort <105147959+lenghort@users.noreply.github.com>
2026-03-12 14:57:35 +0700

    add to site nav,

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -10,12 +10,6 @@
 
     <main class="p-5">
 
-<div class="bg-purple-900 text-white">
-    <ul class="flex space-x-3 p-3">
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('shops.index') }}">Shop</a></li>
-    </ul>
-</div>
         {{ $slot }}
 
     </main>
