d1a598f3ad2d981c9e8c6109a896549ee1bbbd13
lenghort <105147959+lenghort@users.noreply.github.com>
2026-03-13 04:01:06 +0700

    add food selection to create shop form

--- app/Http/Controllers/Home/ShopController.php
+++ app/Http/Controllers/Home/ShopController.php
@@ -38,6 +38,7 @@ class ShopController extends Controller
             'address' => ['required', 'string', 'max:255'],
             'food_type' => ['required', 'string', 'max:255'],
             'price_level' => ['required', 'string', 'max:255'],
+            'foods' => ['array'],
         ]);
 
         // 2. Create the shop
@@ -49,6 +50,7 @@ class ShopController extends Controller
             'price_level' => $data['price_level'],
         ]);
 
+        $shop->foods()->sync($data['foods'] ?? []);
         // 3. Redirect to the shop index page
         return redirect()->route('home.shops.index');
     }
@@ -83,6 +85,7 @@ class ShopController extends Controller
             'address' => ['required', 'string', 'max:255'],
             'food_type' => ['required', 'string', 'max:255'],
             'price_level' => ['required', 'string', 'max:255'],
+            'foods' => ['array'],
         ]);
 
         // 3. update the shop information
@@ -94,6 +97,7 @@ class ShopController extends Controller
             'price_level' => $data['price_level'],
         ]);
 
+        $shop->foods()->sync($data['foods'] ?? []);
         // 4. redirect to the shop index page
         return redirect()->route('home.shops.index');
     }
