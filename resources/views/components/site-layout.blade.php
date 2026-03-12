d51ea9d46f10e0fc4e16fa671100134b5eb098bd
lenghort <105147959+lenghort@users.noreply.github.com>
2026-03-12 15:00:10 +0700

    design welcome using sitelayout

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -1,10 +1,18 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title>List of shops</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
 </head>
 
+<body class="bg-purple-200" style="font-family: 'Instrument-Sans', sans-serif">
 
     <x-site-navigation/>
 
